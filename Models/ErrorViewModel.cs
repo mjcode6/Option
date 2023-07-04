b5a58efdda9ecc555c88b2fb53557655154971f3
sivasuthan manojan <mjcode6@gmail.com>
2023-07-04 18:15:01 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace option.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
